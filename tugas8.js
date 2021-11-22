99eeb4324fcebdc783639323d5ae356070d1a5c4
Fiqrian <fiqrian.faturahman@gmail.com>
2021-11-22 20:06:13 +0700

    tugas 8 javascript

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+const Objek = () => {
+  var i = {
+    nama: "Fiqrian Faturachman",
+    umur: "20 tahun",
+    Kelamin: "Laki - Laki",
+    hobby: ["Menonton Film", "Bermain Game", "Membaca"],
+  };
+  //ubah data properti
+  i.umur = "19 tahun";
+  //
+  console.log(i);
+};
+
+Objek();
